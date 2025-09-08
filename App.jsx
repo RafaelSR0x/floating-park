61ca2efec6193b57ddead221df2fd92cfa156ae8
RafaelSR0x <raffssantana007@gmail.com>
2025-09-08 17:12:42 -0300

    First commit

---
+++ App.jsx
@@ -0,0 +1,15 @@
+import { StatusBar } from 'expo-status-bar';
+import styled from 'styled-components/native';
+import Login from './src/screens/login/index.jsx';
+
+const ContainerApp = styled.SafeAreaView`
+    flex: 1;
+`;
+export default function App() {
+    return (
+        <ContainerApp>
+            <StatusBar hidden />
+            <Login />
+        </ContainerApp>
+    );
+}
